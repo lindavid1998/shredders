721d607b716ab7ffe30e1d2b7358910adc67e1b6
David Lin <lindavid1998@gmail.com>
2024-05-03 08:55:51 -0700

    Modify Home page to fetch all trips that user is part of

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -7,16 +7,64 @@ import { Link } from 'react-router-dom';
 
 const Home = () => {
 	const { user } = useAuth();
+	const [trips, setTrips] = useState(null);
 
 	// const isLoading = useLoad(fetchData);
+	// email: 'test@email.com';
+	// exp: 1714752337;
+	// first_name: 'Foo';
+	// iat: 1714748737;
+	// last_name: 'Bar';
+	// user_id: 29;
 
-	// if (isLoading) {
-	// 	return <Spinner />;
-	// }
+	useEffect(() => {
+		// fetch data
+		const fetchData = async () => {
+			const response = await fetch(
+				`http://localhost:3000/${version}?userId=${user.user_id}`,
+				{
+					method: 'GET',
+					credentials: 'include',
+					headers: {
+						'Content-Type': 'application/json',
+					},
+				}
+			);
+
+			if (!response.ok) {
+				const error = data.errors[0].msg;
+				console.log(error);
+				return;
+			}
+
+			const data = await response.json();
+			console.log(data);
+			setTrips(data);
+		};
+
+		fetchData();
+	}, []);
+
+	if (!trips) {
+		return <Spinner />;
+	}
+
+	if (trips.length == 0) {
+		return <div>You have no trips</div>;
+	}
 
 	return (
 		<>
 			<div>Welcome {user.first_name}</div>
+
+			{trips.map((trip, index) => (
+				<div key={index}>
+					{/* <Card key={index} data={item} /> */}
+					<div>{trip.location}</div>
+					<div>{trip.status}</div>
+				</div>
+			))}
+
 			<Link className='text-blue-600' to={`/`}>
 				Back to landing page
 			</Link>
--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ const apiVersion = process.env.API_VERSION;
 const morgan = require('morgan');
 const authorization = require('./middleware/authorization');
 const cookieParser = require('cookie-parser');
+const pool = require('./db');
 
 // import routers
 const authRouter = require('./routes/jwtAuth');
@@ -26,6 +27,57 @@ const corsOptions = {
 app.use(cors(corsOptions));
 
 // ROUTES
+
+// home page
+app.get(`/${apiVersion}`, authorization, async (req, res) => {
+	const userId = req.query.userId;
+
+	try {
+		const query = `
+			SELECT
+				rsvps.trip_id,
+				rsvps.status,
+				trips.start_date,
+				trips.end_date,
+				destinations.name AS location
+			FROM
+				rsvps
+			INNER JOIN trips
+				ON rsvps.trip_id = trips.trip_id
+			INNER JOIN destinations
+				ON trips.destination_id = destinations.destination_id
+			WHERE
+				rsvps.user_id = $1
+			ORDER BY trips.start_date; 
+		`;
+
+		const result = await pool.query(query, [userId]);
+
+		// [
+		// 	{
+		// 		trip_id: 51,
+		// 		status: 'Going',
+		// 		start_date: 2024-05-01T07:00:00.000Z,
+		// 		end_date: 2024-05-04T07:00:00.000Z,
+		// 		location: 'Mammoth'
+		// 	},
+		// 	{
+		// 		trip_id: 52,
+		// 		status: 'Not going',
+		// 		start_date: 2024-05-02T07:00:00.000Z,
+		// 		end_date: 2024-05-03T07:00:00.000Z,
+		// 		location: 'Bear Mountain'
+		// 	}
+		// ]
+		res.status(200).json(result.rows)
+	} catch (err) {
+		if (typeof err === 'object') {
+			res.status(500).json({ errors: [{ msg: 'Internal server error' }] });
+		} else {
+			res.status(500).json({ errors: [{ msg: err }] });
+		}
+	}
+});
 
 // log in and sign up pages
 app.use(`/${apiVersion}/auth`, authRouter);
