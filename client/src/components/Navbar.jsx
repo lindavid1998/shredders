814d3a25b472eeee5e64862b4b1577af148df1c4
David Lin <lindavid1998@gmail.com>
2024-06-09 18:01:41 -0700

    Collapse dropdown into hamburger menu for narrow screens

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,14 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 const version = import.meta.env.VITE_API_VERSION;
 import { useAuth } from '../hooks/useAuth';
 import { useLocation } from 'react-router-dom';
 import Button from '../components/Button';
 import Avatar from './Avatar';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+const Dropdown = ({ className }) => {
+	const { user, logout } = useAuth();
+
+	return (
+		<div className={`${className} w-24`}>
+			<ul className='flex flex-col items-start'>
+				<h6 className='nav-dropdown-item'>About</h6>
+				<h6 className='nav-dropdown-item'>Pricing</h6>
+
+				{user && (
+					<h6 className='nav-dropdown-item' onClick={logout}>
+						Logout
+					</h6>
+				)}
+
+				{!user && (
+					<h6 className='nav-dropdown-item'>
+						<Link to={`/${version}/auth/login`}>Login</Link>
+					</h6>
+				)}
+
+				{!user && (
+					<h6 className='nav-dropdown-item'>
+						<Link to={`/${version}/auth/signup`}>Sign up</Link>
+					</h6>
+				)}
+			</ul>
+		</div>
+	);
+}
 
 const Navbar = () => {
 	const { user, logout } = useAuth();
 	const { pathname } = useLocation();
+	const [showDropdown, setShowDropdown] = useState(false)
+
+	const toggleDropdown = () => {
+		setShowDropdown(prevState => !prevState)
+	}
 
 	if (pathname.includes('/auth')) return null;
 
@@ -18,7 +56,12 @@ const Navbar = () => {
 				<h1 style={{ fontWeight: 'bold' }}>Shredders</h1>
 			</Link>
 
-			<ul className='flex ml-auto gap-10 items-center'>
+			<div className='md:hidden ml-auto cursor-pointer relative'>
+				<FontAwesomeIcon size='lg' icon={faBars} onClick={toggleDropdown} />
+				{showDropdown && <Dropdown className='absolute right-0' />}
+			</div>
+
+			<ul className='hidden md:flex ml-auto gap-10 items-center'>
 				<h6 className='navbar-item'>About</h6>
 
 				<h6 className='navbar-item'>Pricing</h6>
--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -7,10 +7,10 @@ const Hero = () => {
 	const navigate = useNavigate();
 
 	return (
-		<div className='hero relative w-full flex flex-col justify-center items-center'>
+		<div className='hero relative w-full flex flex-col justify-center items-center md:gap-8'>
 			<div className='hero-img' />
 
-			<div className='hero-text absolute top-14 w-full max-w-screen-xl px-5 mx-auto'>
+			<div className='hero-text md:absolute top-14 w-full max-w-screen-xl px-5 mx-auto'>
 				<div className='max-w-md'>
 					<h2 style={{ fontWeight: 'bold' }}>
 						Plan Your Perfect Snow Adventure
