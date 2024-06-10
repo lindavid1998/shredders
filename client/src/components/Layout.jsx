16b4369228caf967c01c89ef358814b40dd92a4e
David Lin <lindavid1998@gmail.com>
2024-06-09 20:55:00 -0700

    Add footer

--- client/src/components/Layout.jsx
+++ client/src/components/Layout.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Outlet, useLocation } from 'react-router-dom';
 import Navbar from './Navbar';
+import Footer from './Footer';
 
 const Layout = () => {
 	const location = useLocation();
@@ -18,6 +19,7 @@ const Layout = () => {
 			>
 				<Outlet />
 			</div>
+			<Footer />
 		</div>
 	);
 };
