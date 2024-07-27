c917f6c8c4cc720874c69c1af7cd5bb6e1211f3f
David Lin <lindavid1998@gmail.com>
2024-07-26 22:51:24 -0700

    Split RSVPs into columns on larger screens

--- client/src/components/RSVPs.jsx
+++ client/src/components/RSVPs.jsx
@@ -1,33 +1,14 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 import Avatar from './Avatar';
 import { faCheck, faX, faQuestion } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-const RSVPCount = ({ responses }) => {
-	let result = {
-		Going: 0,
-		Tentative: 0,
-		Declined: 0,
-	};
-
-	for (const user of responses) {
-		switch (user.status) {
-			case 'Going':
-				result['Going']++;
-				break;
-			case 'Declined':
-				result['Declined']++;
-				break;
-			default:
-				result['Tentative']++;
-		}
-	}
-
+const RSVPCount = ({ numGoing, numTentative, numDeclined }) => {
 	return (
 		<div>
-			<span className='font-bold'>{result['Going']}</span> accepted,
-			<span className='font-bold'> {result['Tentative']}</span> maybe,
-			<span className='font-bold'> {result['Declined']}</span> declined
+			<span className='font-bold'>{numGoing}</span> accepted,
+			<span className='font-bold'> {numTentative}</span> maybe,
+			<span className='font-bold'> {numDeclined}</span> declined
 		</div>
 	);
 };
@@ -69,20 +50,104 @@ const User = ({ user }) => {
 };
 
 const RSVPs = ({ responses }) => {
-  // handle the case if there are no RSVPs
-  if (!responses || responses.length == 0) {
+	const [isSmallScreen, setIsSmallScreen] = useState(false);
+	const [splitResponses, setSplitResponses] = useState({});
+	const [numGoing, setNumGoing] = useState(0);
+	const [numTentative, setNumTentative] = useState(0);
+	const [numDeclined, setNumDeclined] = useState(0);
+	const [isLoading, setIsLoading] = useState(true);
+
+	useEffect(() => {
+		const widthThreshold = 768; // md screen is 768 px
+		const handleResize = () => {
+			setIsSmallScreen(window.innerWidth < 768); // Adjust the breakpoint as needed
+		};
+
+		handleResize(); // Check on initial render
+
+		window.addEventListener('resize', handleResize);
+		return () => window.removeEventListener('resize', handleResize);
+	}, []);
+
+	useEffect(() => {
+		let splitResponses = {
+			Going: [],
+			Tentative: [],
+			Declined: [],
+		};
+
+		responses.map((response) => {
+			splitResponses[response.status].push(response);
+		});
+
+		setSplitResponses(splitResponses);
+		setNumGoing(splitResponses['Going'].length);
+		setNumTentative(splitResponses['Tentative'].length);
+		setNumDeclined(splitResponses['Declined'].length);
+		setIsLoading(false);
+	}, []);
+
+	if (isLoading) {
+		return <div>Loading...</div>;
+	}
+
+	// handle the case if there are no RSVPs
+	if (!responses || responses.length == 0) {
 		return <div>There are no RSVPs!</div>;
 	}
 
-  return (
+	// on small screen
+	if (isSmallScreen) {
+		return (
+			<div className='section rsvps'>
+				<h3 className='text-center md:text-left'>Who's Coming</h3>
+
+				<RSVPCount
+					numGoing={numGoing}
+					numTentative={numTentative}
+					numDeclined={numDeclined}
+				/>
+
+				{responses.map((user, index) => (
+					<User key={index} user={user} />
+				))}
+			</div>
+		);
+	}
+
+	// on medium screen, render the split users arrays
+	return (
 		<div className='section rsvps'>
 			<h3 className='text-center md:text-left'>Who's Coming</h3>
-			<RSVPCount responses={responses} />
-			{responses.map((user, index) => (
-				<User key={index} user={user} />
-			))}
+
+			<div className='rsvp-columns'>
+				<div className='rsvp-column'>
+					<div>
+						<span className='font-bold'>{numGoing}</span> accepted
+					</div>
+					{splitResponses['Going'].map((user, index) => (
+						<User key={index} user={user} />
+					))}
+				</div>
+				<div className='rsvp-column'>
+					<div>
+						<span className='font-bold'>{numTentative}</span> tentative
+					</div>
+					{splitResponses['Tentative'].map((user, index) => (
+						<User key={index} user={user} />
+					))}
+				</div>
+				<div className='rsvp-column'>
+					<div>
+						<span className='font-bold'>{numDeclined}</span> declined
+					</div>
+					{splitResponses['Declined'].map((user, index) => (
+						<User key={index} user={user} />
+					))}
+				</div>
+			</div>
 		</div>
 	);
-}
+};
 
-export default RSVPs
+export default RSVPs;
