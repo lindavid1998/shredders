d155a766a71a03f4d4af52032f7b58098dc5cc2e
David Lin <lindavid1998@gmail.com>
2024-07-29 07:11:05 -0700

    Prevent users from sending friend requests to each other if they are already friends

--- server/routes/friends.js
+++ server/routes/friends.js
@@ -13,23 +13,84 @@ const doesFriendRequestExist = async (userId1, userId2) => {
 
 		const result = await pool.query(query, [userId1, userId2])
 
-		const numRows = result.rowCount
-
-		return numRows > 0
+		return result.rowCount > 0
 	} catch (error) {
 		console.log(error)
 		return False
 	}
 }
 
-router.post('/add', async (req, res) => {
+const areFriends = async (userId1, userId2) => {
+	try {
+		const query = `
+			SELECT * FROM friends
+			WHERE
+				(user1_id = $1 AND user2_id = $2)
+			OR
+				(user1_id = $2 AND user2_id = $1);
+		`;
+
+		const result = await pool.query(query, [userId1, userId2]);
+
+		return result.rowCount > 0;
+	} catch (error) {
+		console.log(error);
+		return False;
+	}
+};
+
+const handleError = (err, res) => {
+	if (typeof err === 'object' && err.message) {
+		res.status(500).json({ errors: [{ msg: err.message }] });
+	} else if (typeof err === 'object') {
+		res.status(500).json({ errors: [{ msg: 'Internal server error' }] });
+	} else {
+		res.status(500).json({ errors: [{ msg: err }] });
+	}
+};
+
+router.post('/accept/:id', async (req, res) => {
 	try {
 		const fromUserId = req.user.user_id;
-		const toUserId = Number(req.query.user_id);
+		const toUserId = Number(req.params.id);
+
+		// DELETE REQUEST FROM friend_requests
+		const delQuery = `
+			DELETE FROM friend_requests
+			WHERE
+				(from_user_id = $1 AND to_user_id = $2)
+				OR (from_user_id = $2 AND to_user_id = $1)
+			RETURNING *;
+		`;
+		const result = await pool.query(delQuery, [fromUserId, toUserId])
+		if (result.rowCount == 0) {
+			throw new Error('friend request does not exist')
+		}
+
+		// INSERT new row INTO friends
+		const addQuery = `
+			INSERT INTO friends(user1_id, user2_id)
+			VALUES ($1, $2)
+		`
+		await pool.query(addQuery, [fromUserId, toUserId])
+		
+		res.send(200).json('friend request accepted')
+	} catch (error) {
+		handleError(error, res);
+	}
+})
+
+router.post('/add/:id', async (req, res) => {
+	try {
+		const fromUserId = req.user.user_id;
+		const toUserId = Number(req.params.id);
 
 		if (await doesFriendRequestExist(fromUserId, toUserId)) {
-			res.status(200).send('friend request already exists')
-			return
+			throw new Error('friend request already exists')
+		}
+
+		if (await areFriends(fromUserId, toUserId)) {
+			throw new Error('users are already friends')
 		}
 
 		const query = `
@@ -44,12 +105,7 @@ router.post('/add', async (req, res) => {
 
 		res.status(200).json(friendRequestId)
 	} catch (error) {
-		if (typeof err === 'object') {
-			console.log(err);
-			res.status(500).json({ errors: [{ msg: 'Internal server error' }] });
-		} else {
-			res.status(500).json({ errors: [{ msg: error }] });
-		}
+		handleError(error, res);
 	}
 })
 
@@ -69,13 +125,8 @@ router.get('/', async (req, res) => {
 		const friends = result.rows;
 
 		res.status(200).json(friends);
-	} catch (err) {
-		if (typeof err === 'object') {
-			console.log(err);
-			res.status(500).json({ errors: [{ msg: 'Internal server error' }] });
-		} else {
-			res.status(500).json({ errors: [{ msg: err }] });
-		}
+	} catch (error) {
+		handleError(error, res);
 	}
 });
 
