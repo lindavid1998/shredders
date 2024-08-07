b5f6b1dfa1f59614c8331a65633c3670c5b351a2
David Lin <lindavid1998@gmail.com>
2024-08-06 17:39:06 -0700

    Update directory path for client static files

--- server/server.js
+++ server/server.js
@@ -31,7 +31,7 @@ const corsOptions = {
 app.use(cors(corsOptions));
 
 // Serve static files from the React app
-app.use(express.static(path.join(__dirname, 'client/dist')));
+app.use(express.static(path.join(__dirname, '../client/dist')));
 
 // ROUTES
 
@@ -48,7 +48,7 @@ app.use(`/${apiVersion}/avatar`, authorization, avatarRouter);
 
 // The "catchall" handler: for any request that doesn't match one above, send back React's index.html file.
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client/dist', 'index.html'));
+    res.sendFile(path.join(__dirname, '../client/dist', 'index.html'));
 });
 
 app.listen(port, () => {
