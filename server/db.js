df1d6d9bfb382d46ff91bb3c657e5362063438bb
David Lin <lindavid1998@gmail.com>
2024-10-31 14:47:58 -0700

    Add backend server to docker compose

--- server/db.js
+++ server/db.js
@@ -1,10 +1,12 @@
 const { Pool } = require('pg');
+const fs = require('fs');
+const db_pw = fs.readFileSync(process.env.DB_PASSWORD_FILE, 'utf-8');
 
 const pool = new Pool({
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
   database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
+  password: db_pw,
   port: process.env.DB_PORT,
 });
 
