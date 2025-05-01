83ea81e1f31d7a2051c8bcfbe449ccfbb143b110
David Lin <lindavid1998@gmail.com>
2025-04-30 17:33:58 -0700

    Migrate image and object storage from Supabase to S3

--- server/routes/avatar.js
+++ server/routes/avatar.js
@@ -5,6 +5,20 @@ const upload = multer();
 const { createClient } = require('@supabase/supabase-js');
 const { decode } = require('base64-arraybuffer');
 const jwtGenerator = require('../jwtGenerator');
+const {
+	S3Client,
+	PutObjectCommand,
+	GetObjectCommand,
+} = require('@aws-sdk/client-s3');
+
+// Initialize S3 client
+const s3Client = new S3Client({
+  region: process.env.AWS_REGION,
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  },
+});
 
 const supabase = createClient(
 	process.env.SUPABASE_PROJECT_URL,
@@ -20,23 +34,24 @@ router.post('/upload', upload.single('avatar'), async (req, res) => {
 		const file = req.file;
 		const fileType = file.mimetype.split('/')[1];
 
-		// decode file buffer to base64
-		const fileBase64 = decode(file.buffer.toString('base64'));
+		// Generate unique file key
+		const fileKey = `avatars/user_${user_id}_${Date.now()}.${fileType}`;
 
-		// upload file to supabase (with replacement)
-		const { data, error } = await supabase.storage
-			.from('avatars')
-			.upload(`user_${user_id}_${Date.now()}.${fileType}`, fileBase64, {
-				contentType: 'image/jpeg, image/png',
-				upsert: true,
-			});
+		// Put an object into an Amazon S3 bucket.
+		await s3Client.send(
+			new PutObjectCommand({
+				Bucket: process.env.AWS_S3_BUCKET_NAME,
+				Key: fileKey,
+				Body: file.buffer,
+				ContentType: file.mimetype,
+			})
+		);
 
-		// get file URL
-		const { data: image } = supabase.storage
-			.from('avatars')
-			.getPublicUrl(data.path);
-		const url = image.publicUrl;
+		// Generate the public URL for the uploaded file
+		const url = `https://${process.env.AWS_S3_BUCKET_NAME}.s3.${process.env.AWS_REGION}.amazonaws.com/${fileKey}`;
+		console.log(url)
 
+		// update url in users db table
 		await supabase
 			.from('users')
 			.update({ avatar_url: url })
@@ -51,7 +66,7 @@ router.post('/upload', upload.single('avatar'), async (req, res) => {
 			user.email,
 			url
 		);
-		
+
 		res.cookie('token', token, {
 			maxAge: 900000,
 			httpOnly: true,
@@ -59,7 +74,7 @@ router.post('/upload', upload.single('avatar'), async (req, res) => {
 			sameSite: 'None',
 		});
 
-    res.sendStatus(200);
+		res.sendStatus(200);
 	} catch (error) {
 		handleError(error, res);
 	}
